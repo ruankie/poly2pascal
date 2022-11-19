2e8ca1d14e1fa3217eca4bf4f9a993d84a7dbb3f
ruankie <pretoriusr.7@gmail.com>
2022-11-19 16:45:12 +0200

    added sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../poly2pascal"))
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'poly2pascal'
+copyright = '2022, Ruan Pretorius'
+author = 'Ruan Pretorius'
+release = '0.0.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+# use the readthedocs theme
+html_theme = 'sphinx_rtd_theme'
+
+# some parameters to customise the looks of your docs
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#1a4a73',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'titles_only': False
+}
+
+# a logo (delete line if you don't want to display one)
+# html_logo = 'https://link/to/logo.png'
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. poly2pascal documentation master file, created by
+   sphinx-quickstart on Sat Nov 19 16:11:17 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+poly2pascal documentation
+=========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   getting_started
+   annotations
+   data_loader
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
